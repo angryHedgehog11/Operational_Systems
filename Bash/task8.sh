d3129460d14565bf3b715293cf231fea21bc7408
Radostina Hadzhieva <80592563+angryHedgehog11@users.noreply.github.com>
2023-07-26 15:57:51 +0300

    Create task8.sh

---
+++ Bash/task8.sh
@@ -0,0 +1,5 @@
+#!/bin/bash 
+
+GROUP=$(cat /etc/passwd | cut -c 2- | sort -n | head -n 201 | tail -n 1 | cut -d ":" -f 4)
+
+cat /etc/passwd | egrep $GROUP | cut -d ":" -f 5,6 | tr -s "," ":" | cut -d ":" -f 1,5 
