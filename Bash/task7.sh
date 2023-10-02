c58e89471c1c49857d0bf23de99d050feaa43036
Radostina Hadzhieva <80592563+angryHedgehog11@users.noreply.github.com>
2023-10-02 17:15:23 +0300

    Update task7.sh

--- Bash/task7.sh
+++ Bash/task7.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-find $(cat /etc/passwd | cut -d ":" -f 3,6 | egrep "^$(id -u pesho)" | cut -d ":" -f 2) -type f -printf "%n %T@ %i\n" 2>/dev/null | egrep -v "^1" | sort -k 2 -n | head -n 1 | cut -d " " -f 3 
+find $(cat /etc/passwd | cut -d ":" -f 3,6 | egrep "^$(id -u pesho)" | cut -d ":" -f 2) -type f -printf "%n %T@ %i\n" 2>/dev/null | egrep -w "^1" -v | sort -k 2 -n | head -n 1 | cut -d " " -f 3 
