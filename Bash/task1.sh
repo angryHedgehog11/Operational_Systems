a816cc7e3b7b5e6bee131362684fba0f9bb4dbe2
Radostina Hadzhieva <80592563+angryHedgehog11@users.noreply.github.com>
2023-10-02 14:40:00 +0300

    Update task1.sh

--- Bash/task1.sh
+++ Bash/task1.sh
@@ -1,3 +1,3 @@
 #!/bin/bash 
 
-echo "The number of searched rows is: $(egrep '[02468]+' philip-j-fry.txt | egrep -v '[a-w]' | wc -l)"
+echo "The number of searched rows is: $(egrep '[02468]+' philip-j-fry.txt | egrep -v '[a-z]' | wc -l)"
