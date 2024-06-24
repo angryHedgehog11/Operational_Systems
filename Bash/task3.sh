8025d76fb503285388c5100832a1b5d616db5fc9
Radostina Hadzhieva <80592563+angryHedgehog11@users.noreply.github.com>
2024-06-25 00:19:00 +0300

    Update task3.sh

--- Bash/task3.sh
+++ Bash/task3.sh
@@ -1,5 +1,7 @@
 #!/bin/bash 
 
-find . -type f -size 0c 2>/dev/null | xargs rm  
+find . -type f -size 0c 2>/dev/null | xargs rm -rf
 
-find . -type f -printf "%p %s\n" 2>/dev/null | sort -nr -k 2 | cut -d " " -f 1 | head -n 5 | xargs rm
+HOMEDIR=$(egrep $(whoami) /etc/passwd | cut -d : -f 6)
+
+find $HOMEDIR -type f -printf "%p %s\n" 2>/dev/null | sort -nr -k 2 | cut -d " " -f 1 | head -n 5 | xargs rm -rf
