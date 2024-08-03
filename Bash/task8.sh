24d9519c9ff51f420a4854334e60b68863993d54
Radostina Hadzhieva <80592563+angryHedgehog11@users.noreply.github.com>
2024-08-03 16:28:58 +0300

    Update task8.sh

--- Bash/task8.sh
+++ Bash/task8.sh
@@ -2,4 +2,4 @@
 
 GROUP=$(cat /etc/passwd | cut -c 2- | sort -n | head -n 201 | tail -n 1 | cut -d ":" -f 4)
 
-cat /etc/passwd | egrep $GROUP | cut -d ":" -f 5,6 | tr -s "," ":" | cut -d ":" -f 1,5 
+cat /etc/passwd | egrep $GROUP | cut -c 2- | sort -n | cut -d ":" -f 5,6 | tr -s "," ":" | cut -d ":" -f 1,5 
