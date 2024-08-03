58bf8b59640d231d431cf289a22cf49834f4bf6e
Radostina Hadzhieva <80592563+angryHedgehog11@users.noreply.github.com>
2024-08-03 15:51:14 +0300

    Update task5.sh

--- Bash/task5.sh
+++ Bash/task5.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-find . -type f -printf "%n %f\n" 2>/dev/null | sort -nr | cut -d " " -f 2 | head -n 5  
+find . -type f -printf "%n %f\n" 2>/dev/null | sort -k 2 -nr | cut -d " " -f 2 | head -n 5  
